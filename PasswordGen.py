580bb4accb90d5f292a4fd01f45b497bbb3feed7
Brandon Altamirano <manabran501@hotmail.com>
2022-01-02 01:33:47 -0800

    Added checkboxes for requirements of password, no functions yet

--- PasswordGen.py
+++ PasswordGen.py
@@ -16,8 +16,11 @@ all_numbers = 9
 special_chars_num = len(special_chars) - 1
 
 #Window configuration
-window.rowconfigure(4, minsize=500, weight=1)
+window.rowconfigure(5, minsize=150, weight=1)
 window.columnconfigure(3, minsize=600, weight=1)
+
+#Need function to check user requirements for password (Uppercase, numbers, symbols)
+
 
 #Algorithm for complete random password
 def com_rand_password():
@@ -64,6 +67,7 @@ def delete_passwords():
 
 #Frames
 first_row_buttons = tk.Frame(window)
+fr_checkboxes = tk.Frame(window)
 fr_length = tk.Frame(window)
 fr_random = tk.Frame(window)
 fr_hint1 = tk.Frame(window)
@@ -80,13 +84,23 @@ first_row_buttons.grid(row=0, column=0, sticky="ns")
 
 #Second Column Widgets
 
+#Requirements boxes
+check_uppercase = tk.Checkbutton(fr_checkboxes, text='Uppercase', onvalue=True, offvalue=False)
+check_number = tk.Checkbutton(fr_checkboxes, text="Number", onvalue=True, offvalue=False)
+check_symbol = tk.Checkbutton(fr_checkboxes, text="Symbol", onvalue=True, offvalue=False)
+check_uppercase.grid(row=0, column=0, padx=5, pady=5, sticky="ew")
+check_number.grid(row=1, column=0, padx=5, pady=5, sticky="ew")
+check_symbol.grid(row=2, column=0, padx=5, pady=5, sticky="ew")
+
+fr_checkboxes.grid(row=0, column=1, padx=5, pady=5, sticky="nsew")
+
 #Length
 lbl_length = tk.Label(fr_length, text="Length")
 entry_length = tk.Entry(fr_length)
 lbl_length.grid(row=0, column=0, padx=5, sticky="ew", pady=3)
 entry_length.grid(row=1, column=0, padx=5, sticky="ew")
 
-fr_length.grid(row=0, column=1, sticky="nsew")
+fr_length.grid(row=1, column=1, sticky="nsew")
 
 #Random Generate
 lbl_random = tk.Label(fr_random, text="Generate completely random password")
@@ -94,7 +108,7 @@ btn_random_pass = tk.Button(fr_random, text="Generate", command=com_rand_passwor
 lbl_random.grid(row=0, column=0, sticky="ew", padx=5, pady=1)
 btn_random_pass.grid(row=1, column=0, padx=5, pady=5)
 
-fr_random.grid(row=1,column=1,sticky="nsew")
+fr_random.grid(row=2,column=1,sticky="nsew")
 
 #Hint 1 Widgets
 lbl_hint1 = tk.Label(fr_hint1, text="Hint 1")
@@ -102,7 +116,7 @@ entry_hint1 = tk.Entry(fr_hint1)
 lbl_hint1.grid(row=0, column=0, sticky="ew", padx=5, pady=3)
 entry_hint1.grid(row=1, column="0", sticky="ew", padx=5,)
 
-fr_hint1.grid(row=2, column=1, sticky="nsew")
+fr_hint1.grid(row=3, column=1, sticky="nsew")
 
 #Hint 2 Widgets
 lbl_hint2 = tk.Label(fr_hint2, text="Hint 2")
@@ -110,7 +124,7 @@ entry_hint2 = tk.Entry(fr_hint2)
 lbl_hint2.grid(row=0, column=0, sticky="ew", padx=5, pady=3)
 entry_hint2.grid(row=1, column="0", sticky="ew", padx=5,)
 
-fr_hint2.grid(row=3, column=1, sticky="nsew")
+fr_hint2.grid(row=4, column=1, sticky="nsew")
 
 #Hint 3 Widgets
 lbl_hint3 = tk.Label(fr_hint3, text="Hint 3")
@@ -118,7 +132,7 @@ entry_hint3 = tk.Entry(fr_hint3)
 lbl_hint3.grid(row=0, column=0, sticky="ew", padx=5, pady=3)
 entry_hint3.grid(row=1, column="0", sticky="ew", padx=5,)
 
-fr_hint3.grid(row=4, column=1, sticky="nsew")
+fr_hint3.grid(row=5, column=1, sticky="nsew")
 
 #Password Generator
 entry_password = tk.Entry(fr_password)
